32a8833b7b916d862203c5e01015ec85bdbb5594
kurabiyecanavari1903 <bilalozturk049@gmail.com>
2026-08-27 21:09:18 +0300

    bitti

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,25 +1,52 @@
-import React, { useRef, useState } from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import todo_icon from '../assets/todo_icon.png'
 import TodoItems from './TodoItems'
 
 const Todo = () => {
 
-const [todoList, setTodoList] = useState([]);
+const [todoList, setTodoList] = useState(localStorage.getItem("todos")? JSON.parse(localStorage.getItem("todos")) : []);
 
 const inputRef = useRef(); 
 
 const ekle = () => {
     const inputText = inputRef.current.value.trim();
 
+    if (inputText === "") {
+        return null;
+    }
+
     const newTodo = {
-        id: date.now(),
+        id: Date.now(),
         text: inputText,
         isComplete: false,
     }
     setTodoList((prev) => [...prev, newTodo]);
+    inputRef.current.value = "";
 }
 
-  return (
+const deleteTodo = (id) => {
+    setTodoList((prvTodos) => {
+       return prvTodos.filter((todo) => todo.id !== id)
+    })
+}
+
+const toggle = (id) => {
+    setTodoList((prevTodos) => {
+        return prevTodos.map((todo) => {
+            if(todo.id === id){
+                return {...todo, isComplete: !todo.isComplete} 
+            }
+            return todo;
+
+        })
+    })
+}
+
+useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todoList));
+},[todoList])
+
+return (
     <div className='bg-gray-900 place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-137.5 rounded-xl'>
 
 {/* ------ title kısmı ------ */}
@@ -40,8 +67,10 @@ const ekle = () => {
 {/* ------ todo list kısmı ------ */}
 
     <div>
-        <TodoItems text="Koşuya Çıkıyorum"/>
-        <TodoItems text="Proje Ödevini Yapıyorum"/>
+        {todoList.map((item, index) => {
+            return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} toggle={toggle} />
+        })}
+       
     </div>
 
     
--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -3,15 +3,15 @@ import tick from '../assets/tick.png'
 import not_tick from '../assets/not_tick.png'
 import delete_icon from '../assets/delete.png'
 
-const TodoItems = ({text}) => {
+const TodoItems = ({text, id, isComplete, deleteTodo, toggle}) => {
   return (
-    <div className='flex items-center my-3 gap-2'>
+    <div onClick={() => {toggle(id)}} className='flex items-center my-3 gap-2'>
         <div className='flex flex-1 items-center cursor-pointer'>
-            <img src={tick} alt="" className='w-7' />
-            <p className='text-shadow-slate-700 ml-4 text-[16px] text-white'>{text}</p>
-        </div>
+            <img src={isComplete ? tick : not_tick} alt="" className='w-7' />
+           <p className={`text-shadow-slate-700 ml-4 text-[16px] decoration-red-600 text-white ${isComplete ? "line-through decoration-2" : ""}`}>{text}</p>
+           </div>
 
-        <img src={delete_icon} alt="" className='w-3.5 cursor-p'/>
+        <img onClick={() => {deleteTodo(id)}} src={delete_icon} alt="" className='w-3.5 cursor-pointer'/>
 
 
     </div>
